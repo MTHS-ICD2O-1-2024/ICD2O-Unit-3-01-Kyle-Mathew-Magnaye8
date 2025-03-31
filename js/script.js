7547768d2fc0892408a2ad653c48103bbe4e7dcf
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-03-31 14:00:18 +0000

    added JS work

--- js/script.js
+++ js/script.js
@@ -3,20 +3,3 @@
 // Created by: kyle.matthew.magnaye
 // Created on: Mar 2025
 // This file contains the JS functions for index.html
-
-// script.js
-function calculateArea() {
-  const a = parseFloat(document.getElementById("base1").value)
-  const b = parseFloat(document.getElementById("base2").value)
-  const h = parseFloat(document.getElementById("height").value)
-
-  if (isNaN(a) || isNaN(b) || isNaN(h)) {
-    document.getElementById("user-info").innerText =
-      "Please enter valid numbers."
-    return
-  }
-
-  const area = ((a + b) / 2) * h
-  document.getElementById("user-info").innerText =
-    "Area: " + area.toFixed() + "mm²"
-}
